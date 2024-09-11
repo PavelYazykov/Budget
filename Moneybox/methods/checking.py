4c3ce40c01fc8a2c66a6446ef45678cd38cf728d
Pavel <y.pawel@mail.ru>
2024-09-11 17:57:39 +0300

    initial_7

--- Moneybox/methods/checking.py
+++ Moneybox/methods/checking.py
@@ -9,6 +9,6 @@ class Checking:
     @staticmethod
     def check_statuscode(result, status_code):
         with allure.step('Проверка статус кода'):
-            assert result.status_code == status_code
+            assert result.status_code == status_code, f"Ошибка {result.text}"
             print(f'Успешно статус код: {result.status_code}')
 
